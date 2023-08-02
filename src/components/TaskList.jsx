1aab2b0238766e1cc3aebde055eb48a6cb4b6e38
wkarol <122057880+karolgalindo02@users.noreply.github.com>
2023-08-01 21:58:53 -0500

    change tasklist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,23 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Checkbox from "./Checkbox";
 
 const TaskList = (props) => {
+  // (C)
   const { list, setList } = props;
 
   const onChangeStatus = (e) => {
     const { name, checked } = e.target;
-
-    const updateList = list.map((item) => ({
-      ...item,
-      done: item.id === name ? checked : item.done,
-    }));
-    setList(updateList);
-  };
-
-  const onClickRemoveItem = (e) => {
-    const updateList = list.filter((item) => !item.done);
-    setList(updateList);
-  };
+    
 /*
   const [isSoundPlaying, setIsSoundPlaying] = useState(false);
 
@@ -35,16 +25,32 @@ const TaskList = (props) => {
     playSound();
   };
 */
+    // (E)
+    const updateList = list.map((item) => ({
+      ...item,
+      done: item.id === name ? checked : item.done,
+    }));
+    setList(updateList);
+  };
+
+  // (D)
+  const onClickRemoveItem = (e) => {
+    const updateList = list.filter((item) => !item.done);
+    setList(updateList);
+  };
+
+  // (A-2)
   const chk = list.map((item) => (
     <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
   ));
   return (
     <div className="todo-list">
+      {/*(A-1)*/}
       {list.length ? chk : "No tasks"}
+      {/*(B)*/}
       {list.length ? (
         <p>
-          {}
-          <button className="button blue" onClick={handleDeleteDoneClick}>
+          <button className="button blue" onClick={onClickRemoveItem}>
             Delete all done
           </button>
         </p>
