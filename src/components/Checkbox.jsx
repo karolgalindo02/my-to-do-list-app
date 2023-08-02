68dea8becfe0dbfc592f067aeee9bb225ca20348
wkarol <122057880+karolgalindo02@users.noreply.github.com>
2023-08-01 21:43:50 -0500

    update components

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from "react";
-import AudioPlayer from "react-audio-player";
 
 const Checkbox = (props) => {
   const {
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TaskList from "./TaskList";
 import FormTodo from "./FormTodo";
 
